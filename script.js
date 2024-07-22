6f1420768aea9a4f2f7332091c49f03666a837a1
Shiqi Wang <thisisshiki@outlook.com>
2024-07-22 11:21:38 +0800

    Update script.js
    update function version

--- script.js
+++ script.js
@@ -1,54 +1,47 @@
-const myLibrary = [];
-
-class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
-
-    toggleRead() {
-        this.read = !this.read;
-    }
-}
-
-function addBookToLibrary(title, author, pages, read) {
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  
+  Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+  }
+  
+  const myLibrary = [];
+  
+  function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
-}
-
-function displayLibrary() {
+  }
+  
+  function displayLibrary() {
     const libraryElement = document.getElementById('library');
     libraryElement.innerHTML = '';
-
     myLibrary.forEach((book, index) => {
-        const bookCard = document.createElement('div');
-        bookCard.classList.add('book-card');
-        bookCard.setAttribute('data-index', index);
-
-        bookCard.innerHTML = `
-            <h3>${book.title}</h3>
-            <p>Author: ${book.author}</p>
-            <p>Pages: ${book.pages}</p>
-            <p>Read: ${book.read ? 'Yes' : 'No'}</p>
-            <button onclick="removeBook(${index})">Remove</button>
-            <button onclick="toggleReadStatus(${index})">Toggle Read Status</button>
-        `;
-
-        libraryElement.appendChild(bookCard);
+      const bookCard = document.createElement('div');
+      bookCard.classList.add('book-card');
+      bookCard.setAttribute('data-index', index);
+      bookCard.innerHTML =   `<h3>${book.title}</h3>
+                               <p>Author: ${book.author}</p>
+                               <p>Pages: ${book.pages}</p>
+                               <p>Read: ${book.read ? 'Yes' : 'No'}</p>
+                               <button onclick="removeBook(${index})">Remove</button>
+                               <button onclick="toggleReadStatus(${index})">Toggle Read Status</button>`;
+      libraryElement.appendChild(bookCard);
     });
-}
-
-function removeBook(index) {
+  }
+  
+  function removeBook(index) {
     myLibrary.splice(index, 1);
     displayLibrary();
-}
-
-function toggleReadStatus(index) {
+  }
+  
+  function toggleReadStatus(index) {
     myLibrary[index].toggleRead();
     displayLibrary();
-}
+  }
 
 // Event Listeners
 document.getElementById('newBookBtn').addEventListener('click', () => {
